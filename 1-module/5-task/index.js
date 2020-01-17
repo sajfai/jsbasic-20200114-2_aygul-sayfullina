8027f6a53fbfa8ab2dbb71c4755192e1421e1ce2
Aygul Sayfullina <sajfai@skbkontur.ru>
2020-01-17 16:59:22 +0500

    Задача 5 модуля 1 "Усечение строки".

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,11 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-
+  let strLength = str.length;
+  let threeDotsSimbol = "…";
+  if (strLength >= maxlength) {
+    return str.slice(0, maxlength - 1) + threeDotsSimbol;
+  } else {
+    return str;
+  }
 }
