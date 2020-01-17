9728f752d89f21d620fe3a28f950c773c976519d
Айгуль Сайфуллина <sayfullinaai@gmail.com>
2020-01-17 14:00:26 +0500

    вторая задачка.

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,15 @@
  * @returns {number}
  */
 function factorial(n) {
-
+	if (n == 0 || n == 1) 
+	{
+		return 1;
+	} else {
+		let firstMultiplier = n;
+		let factorial = 1;
+		for (let i = 1; i < n; i++) {
+		  factorial = factorial * (n - i);
+		}
+		return firstMultiplier * factorial;
+	}
 }
