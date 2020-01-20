21690cb45c68a9807bfb9e652283591aa48a5195
Aygul Sayfullina <sajfai@skbkontur.ru>
2020-01-20 17:29:17 +0500

    Задача 1из модуля 1 "Функция sumSalary(salaries)".

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,11 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-
+  let sum = 0;
+  for (let salary in salaries) {
+    if (Number(salaries[salary])) {
+      sum += Number(salaries[salary]);
+    }
+  }
+  return sum;
 }
