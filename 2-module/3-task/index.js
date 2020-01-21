640d53900b7f02b6c6591082f68c4cc860770514
Aygul Sayfullina <sajfai@skbkontur.ru>
2020-01-21 20:41:01 +0500

    formatting + упростила.

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,14 +1,14 @@
 let calculator = {
-  number1: 0,
-  number2: 0,
-
   read(a, b) {
-    this.number1 = a;
-    this.number2 = b;},
-
-sum() {return (Number(this.number1) + Number(this.number2));},
-
-mul() {return (Number(this.number1) * Number(this.number2));},
+    this.a = a;
+    this.b = b;
+  },
+  sum: function () {
+    return this.a + this.b
+  },
+  mul: function () {
+    return this.a * this.b
+  },
 };
 
 
