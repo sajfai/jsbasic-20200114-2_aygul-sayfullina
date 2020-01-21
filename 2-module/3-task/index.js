fa33e618fcd81834ba192a02410d82edd475dead
Aygul Sayfullina <sajfai@skbkontur.ru>
2020-01-21 17:16:24 +0500

    Задача 3 модуля 2 "Создайте калькулятор".

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,16 @@
 let calculator = {
-  // ваш код
+  number1: 0,
+  number2: 0,
+
+  read(a, b) {
+    this.number1 = a;
+    this.number2 = b;},
+
+sum() {return (Number(this.number1) + Number(this.number2));},
+
+mul() {return (Number(this.number1) * Number(this.number2));},
 };
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
