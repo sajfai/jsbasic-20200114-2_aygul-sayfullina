dc1e7ef0ec5a018211148b71405d4ce6bed0bf4b
Aygul Sayfullina <sajfai@skbkontur.ru>
2020-01-23 18:09:16 +0500

    Задача 2 модуля 3 "Найти min/max в строке".

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,9 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let newStr = str
+    .split(/ |,/)
+    .filter(item => (!isNaN(parseFloat(item))))
+    .map(item => parseFloat(item, 10));
+  return {min: Math.min(...newStr), max: Math.max(...newStr)};
 }
