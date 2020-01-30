57812e0f0d274001719ea8e775c49312ac851ebe
Aygul Sayfullina <sajfai@skbkontur.ru>
2020-01-30 18:29:42 +0500

    Задача 1 модуля 4 "Функция makeFriendsList(friends)".

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,10 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let list = friends
+    .map(item => `<li>${item.firstName} ${item.lastName}</li>`);
+  let ul = document.createElement("ul");
+  ul.innerHTML = list.join('');
+  return ul;
 }
+
