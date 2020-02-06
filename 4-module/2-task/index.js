b0ac6abb0b640a9b88ee129050d47e85285d298f
Aygul Sayfullina <sajfai@skbkontur.ru>
2020-02-06 14:38:15 +0500

    Задача 2 модуля 4 "Выделите ячейки по диагонали"

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,10 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  let rowCount = table.rows.length;
+  let diag = [];
+  for (let i = 0; i < rowCount; i++) {
+    diag.push(table.rows[i].cells[i]);
+  }
+  diag.map(item => item.style.backgroundColor = "red");
 }
