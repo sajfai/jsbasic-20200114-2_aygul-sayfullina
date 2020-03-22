00440de1711c9ba83cdb07628b0defe6e262363c
Aygul Sayfullina <sajfai@skbkontur.ru>
2020-03-22 15:04:54 +0500

    Задача 1 модуля 5 "Функция highlight".

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,24 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let tr of table.querySelectorAll('tbody tr')) {
+    const [, ageTd, genderTd, statusTd] = tr.children;
+    if (statusTd.hasAttribute("data-available")) {
+      if (statusTd.getAttribute("data-available") === "true") {
+        tr.classList.add("available");
+      } else {
+        tr.classList.add("unavailable");
+      }
+    } else {
+      tr.setAttribute("hidden", "true");
+    }
+    if (genderTd.innerHTML === "m") {
+      tr.classList.add("male");
+    } else {
+      tr.classList.add("female");
+    }
+    if (ageTd.innerHTML < 18) {
+      tr.setAttribute("style", "text-decoration: line-through;");
+    }
+  }
 }
