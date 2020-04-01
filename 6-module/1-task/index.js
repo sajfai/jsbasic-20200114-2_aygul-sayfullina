f7857bfaaa9292247f076afd2dcd656a1b639926
Aygul Sayfullina <sajfai@skbkontur.ru>
2020-04-01 21:07:54 +0500

    функции getTable конфликтовали между тестами.

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -16,7 +16,7 @@
 class ClearedTable {
   constructor(data) {
     this.el = document.createElement('table');
-    this.el.innerHTML = getTable(data);
+    this.el.innerHTML = getTable1(data);
     this.el.classList.add("pure-table");
     const table = this.el;
     const x = this;
@@ -36,7 +36,7 @@ class ClearedTable {
   onRemoved(id) {}
 }
 
-function getTable(items) {
+function getTable1(items) {
   const html = [`<thead><tr><td>name</td><td>age</td><td>salary</td><td>city</td><td></td></tr></thead>`];
   for (let i = 0; i < items.length; i++) {
     html.push(`<tbody><tr data-id="${items[i].id}"><td>${items[i].name}</td><td>${items[i].age}</td><td>${items[i].salary}</td><td>${items[i].city}</td><td><a href="#delete">X</a></td></tr></tbody>`
